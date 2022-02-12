be9425b70da327ef7be50627c9439ba73407418f
manuelseet <manuelseet@gmail.com>
2022-02-12 14:54:46 +0800

    First test commit

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
 
+
 export default function AdminDashboard() {
   return (
-    <div>AdminDashboard</div>
+    <div>AdminDashboard
+      <p> Pie Chart Here</p>
+
+      <p> Trend line graph here</p>
+
+
+
+    </div>
+
+
+
+
+
+
+
   )
 }
