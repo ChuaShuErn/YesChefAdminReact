422ab8d023fbd407fb73882ff0d30e18773d6229
manuelseet <manuelseet@gmail.com>
2022-02-17 19:38:40 +0800

    Major changes to dashboard data transfer

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -13,6 +13,7 @@ export default function AdminDashboard() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
+  let content = <p>Found no recipes</p>;
 
   useEffect(() => {
     fetchAllRecipesHandler()
@@ -30,43 +31,112 @@ export default function AdminDashboard() {
 
           setRecipes(data.map(recipe => ({
               ...recipe,
-              searchString: (recipe.name + ' ' +
-                  recipe.cuisineType.reduce((str, cuisine) => str += cuisine + ' ', '') + recipe.courseType + ' ' +
-                  recipe.difficulty.reduce((str, difficultyLevel) => str += difficultyLevel + ' ', '') +
-                  recipe.tags.reduce((str, tag) => str += tag + ' ', '') +
-                  recipe.ingredients.reduce((str, ingredientObj) => str += ingredientObj.ingredient + ' ', '')).toLowerCase()
+              searchString: (recipe.name).toLowerCase()
           })));
+
       } catch (error: any) {
           setError(error.message);
       }
       setIsLoading(false);
 
   };
+
+
+  type groupType = {
+      [key: string]: RecipeType[]
+  }
+
+  const groupByCuisine: groupType = recipes.reduce((group: groupType, recipe: RecipeType) => {
+    const { cuisineType } = recipe;
+    const cuisineValue = cuisineType[0];
+    group[cuisineValue] = group[cuisineValue] ?? [];
+    group[cuisineValue].push(recipe);
+    return group;
+  }, {});
+
+  var cuisine_list = Object.keys(groupByCuisine);
+  var cuisine_data = [];
+  var total_recipe_count = recipes.length;
+  var most_common_cuisine = "";
+  var current_count = 0;
+
+  for (var i = 0; i < cuisine_list.length; i++) {
+    if (groupByCuisine[cuisine_list[i]].length > 5) {
+      var cuisine_count = groupByCuisine[cuisine_list[i]].length;
+      var cuisine_count_dict = {name: cuisine_list[i], value: cuisine_count }
+      cuisine_data.push(cuisine_count_dict);
+      if (cuisine_count > current_count) {
+        current_count = cuisine_count;
+        most_common_cuisine = cuisine_list[i];
+      }
+      
+
+    }
+  }
+
+
+
+    
+  const cuisine_views = [
+    {day: '8 Feb', Thai: 8, Vietnamese: 9, Korean: 7, Japanese: 10, Chinese: 12, British: 11, Indian: 8, American: 14},
+    {day: '9 Feb', Thai: 9, Vietnamese: 11, Korean: 5, Japanese: 9, Chinese: 14, British: 12, Indian: 9, American: 12},
+    {day: '10 Feb', Thai: 7, Vietnamese: 13, Korean: 12, Japanese: 11, Chinese: 13, British: 13, Indian: 11, American: 13},
+    {day: '11 Feb', Thai: 9, Vietnamese: 11, Korean: 13, Japanese: 12, Chinese: 9, British: 11, Indian: 10, American: 10},
+    {day: '12 Feb', Thai: 10, Vietnamese: 14, Korean: 14, Japanese: 14, Chinese: 15, British: 15, Indian: 12, American: 14},
+    {day: '13 Feb', Thai: 12, Vietnamese: 13, Korean: 11, Japanese: 15, Chinese: 18, British: 16, Indian: 13, American: 14},
+    {day: '14 Feb', Thai: 11, Vietnamese: 15, Korean: 16, Japanese: 17, Chinese: 6, British: 12, Indian: 12, American: 16}
+];
+
+
+  const usership_data = [
+    {day: '1 Feb', Users: 1, Views: 2},
+    {day: '2 Feb', Users: 3, Views: 10},
+    {day: '3 Feb', Users: 6, Views: 56},
+    {day: '4 Feb', Users: 2, Views: 80},
+    {day: '5 Feb', Users: 2, Views: 108},
+    {day: '6 Feb', Users: 3, Views: 132},
+    {day: '7 Feb', Users: 4, Views: 206},
+    {day: '8 Feb', Users: 8, Views: 298},
+    {day: '9 Feb', Users: 9, Views: 302},
+    {day: '10 Feb', Users: 5, Views: 369},
+    {day: '11 Feb', Users: 8, Views: 490},
+    {day: '12 Feb', Users: 5, Views: 555},
+    {day: '13 Feb', Users: 3, Views: 666},
+    {day: '14 Feb', Users: 9, Views: 681}
+];
   
-  
+
+
+  if (cuisine_data.length > 0) {
+    content = <Dashboard 
+                cuisine_data={cuisine_data} 
+                cuisine_views={cuisine_views} 
+                usership_data={usership_data} 
+                total_recipe_count = {total_recipe_count}
+                most_common_cuisine = {most_common_cuisine}/>
+  }
+
+  if (error) {
+    content = <p>{error}</p>
+  }
+
+  if (isLoading) {
+      content = <p>Loading...</p>
+  }
+
+
+  console.log("about to return");
   return (
-    
-    
-    
-    
+
     <div> 
       <LeftMenuBar/>
+          <div className="dashboard-indent">
 
+            <h2> <strong>Admin Dashboard </strong>  </h2>
+            {content}
 
-        <div className="dashboard-indent">
-
-          <h2> <strong>Admin Dashboard </strong>  </h2>
-
-          <Dashboard/>
         </div>
 
     </div>
-
-
-
-
-
-
-
   )
 }
