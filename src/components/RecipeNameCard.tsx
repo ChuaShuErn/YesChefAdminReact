a0dc7331f667f441f0161b164143355b7234e436
ChuaShuErn <shuern.chua@gmail.com>
2022-02-22 14:58:31 +0800

    Changed default null image

--- src/components/RecipeNameCard.tsx
+++ src/components/RecipeNameCard.tsx
@@ -41,10 +41,10 @@ const RecipeNameCard = ({ imageURL, name, courseType, difficulty,id }: schema) =
 
   return (
     <div className={classes.container}>
-      <img src={imageURL.slice(-1)[0] ?? "https://yeschefbucket.s3.ap-southeast-1.amazonaws.com/pork_rice.jpg"} 
+      <img src={imageURL.slice(-1)[0] ?? "https://res.cloudinary.com/teepublic/image/private/s--LFwSK24a--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_000000,e_outline:35/co_000000,e_outline:inner_fill:35/co_ffffff,e_outline:35/co_ffffff,e_outline:inner_fill:35/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_auto,h_630,q_90,w_630/v1590476164/production/designs/10604158_0.jpg"} 
       onError={({ currentTarget }) => {
           currentTarget.onerror = null;
-          currentTarget.src = "https://yeschefbucket.s3.ap-southeast-1.amazonaws.com/pork_rice.jpg";
+          currentTarget.src = "https://res.cloudinary.com/teepublic/image/private/s--LFwSK24a--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_000000,e_outline:35/co_000000,e_outline:inner_fill:35/co_ffffff,e_outline:35/co_ffffff,e_outline:inner_fill:35/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_auto,h_630,q_90,w_630/v1590476164/production/designs/10604158_0.jpg";
         }} alt="img alt text" style={{ borderRadius:40, width: 150, height: 150 }} />
         <div className={classes.container__text}> 
         <div className={classes.body} > {name} </div>
